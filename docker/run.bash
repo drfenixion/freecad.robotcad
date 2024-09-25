e0e4c0739f4ab01bf5fa2c6713f666c87947dcb3
drfenixion <gutskombayner@gmail.com>
2024-09-25 17:27:40 +0300

    Add autoinstall debugpy in Debug mode

--- docker/run.bash
+++ docker/run.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Vars
-custom_fc_appimage=FreeCAD-0.21.2-Linux-x86_64.AppImage
+custom_fc_appimage=FreeCAD_1.0.0RC2-conda-Linux-x86_64-py311.AppImage
 custom_command="./../freecad/freecad_custom_appimage_dir/$custom_fc_appimage --appimage-extract-and-run"
 use_default_stable_freecad=true # set false if you want to use some other FreeCAD than default stable FreeCAD (default FC starts faster).
 # Dont forget place FC to docker/freecad/freecad_custom_appimage_dir in that case and fix $custom_fc_appimage variable value
@@ -72,7 +72,6 @@ echo ''
 
 if [ "$use_default_stable_freecad" = false ] ; then
     command=$custom_command
-    force_run_new_container=true
 fi
 echo "Command to run in container: $command"
 
--- freecad/cross/__init__.py
+++ freecad/cross/__init__.py
@@ -1,12 +1,15 @@
 """Entry point of the CROSS workbench."""
 
 import os
+import sys
 
 import FreeCAD as fc
+from freecad.utils import get_python_exe
 
 from .ros.utils import add_ros_library_path
 from .version import __version__
 from .wb_globals import g_ros_distro
+from .freecad_utils import pip_install
 
 add_ros_library_path(g_ros_distro)
 
@@ -18,12 +21,24 @@ if is_ros_found():
 
 # Initialize debug with debugpy.
 if os.environ.get('DEBUG'):
+    print('DEBUG attaching...')
     # how to use:
     # DEBUG=1 command_to_run_freecad
-    # Don't forget to install debugpy.
+    # Use Ubuntu or install manually debugpy module FreeCAD python and FreeCAD python version to OS.
+    # turn on Debugger in VSCODE and add breakpoints to code
     # Cf. https://github.com/FreeCAD/FreeCAD-macros/wiki/Debugging-macros-in-Visual-Studio-Code.
-    import debugpy
-    debugpy.configure(python="python3")
-    debugpy.listen(("0.0.0.0", 5678))
-    debugpy.trace_this_thread(True)
-    debugpy.debug_this_thread()
+
+    def attach_debugger():
+        import debugpy
+        debugpy.configure(python=get_python_exe())
+        debugpy.listen(("0.0.0.0", 5678))
+        debugpy.trace_this_thread(True)
+        debugpy.debug_this_thread()
+        print('DEBUG attached.')
+
+    try:
+        attach_debugger()
+    except:
+        # needed restart freecad after install debugpy
+        pip_install('debugpy', restart_freecad = True)
+        print('Doing restart FreeCAD...')
