a3db94e6814b0d88ad7ef71a6600bc382a8ac903
drfenixion <gutskombayner@gmail.com>
2025-01-23 11:42:39 +0300

    pre-commit hook fix

--- freecad/cross/ui/command_new_controller.py
+++ freecad/cross/ui/command_new_controller.py
@@ -3,7 +3,7 @@ import FreeCADGui as fcgui
 from ..gui_utils import tr
 from ..wb_utils import is_controller_selected, is_robot_selected
 from ..wb_utils import git_init_submodules
-from .dynamic_ui.controllers_selector import ControllersSelectorModalClass 
+from .dynamic_ui.controllers_selector import ControllersSelectorModalClass
 from ..freecad_utils import message
 
 
@@ -15,12 +15,14 @@ class _NewControllerCommand:
             'Pixmap': 'controller.svg',
             'MenuText': tr('Create a Controller or Broadcaster'),
             'Accel': 'N, C',
-            'ToolTip': tr('Create a Controller or Broadcaster.\n'
-                          '\n'
-                          'Controllers are needed to control joints.\n'
-                          'Broadcasters are needed for receive and transfer data from sensors.\n'
-                          '\n'
-                          'See ros2_controllers and ros2_control documentation.'),
+            'ToolTip': tr(
+                'Create a Controller or Broadcaster.\n'
+                '\n'
+                'Controllers are needed to control joints.\n'
+                'Broadcasters are needed for receive and transfer data from sensors.\n'
+                '\n'
+                'See ros2_controllers and ros2_control documentation.',
+            ),
         }
 
     def IsActive(self):
