effe5c5c9dc55c2dcde9393c020b9ba40a76dd6e
drfenixion <gutskombayner@gmail.com>
2025-02-10 04:15:25 +0300

    fix comment

--- freecad/cross/ui/command_set_placement_fast_sensor.py
+++ freecad/cross/ui/command_set_placement_fast_sensor.py
@@ -65,7 +65,7 @@ class _SetCROSSPlacementFastSensorCommand:
         # correct mountedPlacement of sensor link
         set_placement_fast(joint_origin = False)
         # after correction link can have x-positive or x-negative direction dependent of object form
-        # user should correct MountedPlacement if wrong directen set
+        # user should corrects MountedPlacement if wrong direction is set
 
 
 fcgui.addCommand('SetCROSSPlacementFastSensor', _SetCROSSPlacementFastSensorCommand())
