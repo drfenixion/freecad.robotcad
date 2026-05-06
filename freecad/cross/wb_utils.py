4d4685ed0d6e470f684458a9ef4af8b8bc4ba316
drfenixion <it.project.devel@gmail.com>
2026-05-06 15:11:45 +0300

    change displaying of link name in joint Parent/Child to Label instead of Label2. No need to set Label2 with custom joint creating anymore. It is also fix joint (Parent/Child list) error with identic Label2 of link.

--- freecad/cross/wb_utils.py
+++ freecad/cross/wb_utils.py
@@ -516,10 +516,10 @@ def ros_name(obj: DO) -> str:
         or (not obj.isDerivedFrom('App::DocumentObject'))
     )):
         return 'not_a_FreeCAD_object'
+    if obj.Label:
+        return obj.Label    
     if obj.Label2:
         return obj.Label2
-    if obj.Label:
-        return obj.Label
     return obj.Name
 
 
